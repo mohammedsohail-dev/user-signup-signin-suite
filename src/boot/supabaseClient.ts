e844dce3c449a5e523e79dbdffdc1f0de7e90c1c
Mahalti Mohammed Sohail <75085444+mohammedsohail-dev@users.noreply.github.com>
2025-04-28 00:50:49 -0400

    Update supabaseClient.ts

--- src/boot/supabaseClient.ts
+++ src/boot/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://accimeckdxnfotrlpmlv.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjY2ltZWNrZHhuZm90cmxwbWx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1MjMyNDQsImV4cCI6MjA1OTA5OTI0NH0.PP3QW7TL77VNRtKlGyNewe_LLACLuGXR2HcYldUgKaM'
+const supabaseUrl = ''
+const supabaseKey = ''
 export const supabase = createClient(supabaseUrl, supabaseKey)
